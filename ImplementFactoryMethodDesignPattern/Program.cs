a2801c4f9afde2fedb1d8519bcbeb2ceabd4398a
Arushi Gambhir <aArushi@Engineering.ukho.gov.uk>
2021-03-26 13:13:50 +0000

    WIP

---
+++ ImplementFactoryMethodDesignPattern/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ImplementFactoryMethodDesignPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
